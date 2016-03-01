9a1fbbbcb2b6ab35bf292be14ab694427c3820d0
Joe Haig <joe.haig@bbc.co.uk>
2016-03-01 16:59:11 +0000

    Do not override device_type passed in

--- lib/mind_meld/tv.rb
+++ lib/mind_meld/tv.rb
@@ -2,7 +2,7 @@ require 'mind_meld/device'
 
 class MindMeld::Tv < MindMeld::Device
   def initialize options
-    options[:device][:device_type] = 'Tv'
+    options[:device][:device_type] ||= 'Tv'
     super options
   end
 
